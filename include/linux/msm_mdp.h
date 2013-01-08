abf99fd40a7f3f01374974431d2ca64efedbd847
Ken Zhang <kenz@codeaurora.org>
2013-01-08 14:28:20 -0500

    msm: display: panel frame rate report
    Add metadata ioctl for retrieving panel frame rate.
    Get ready to remove reserved fields usage.
    
    Change-Id: I51a4aed9d85efd5f83a184100b6a3dc682c7a67c
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -74,6 +74,8 @@
 #define MSMFB_METADATA_SET  _IOW(MSMFB_IOCTL_MAGIC, 162, struct msmfb_metadata)
 #define MSMFB_DISPLAY_COMMIT      _IOW(MSMFB_IOCTL_MAGIC, 164, \
 						struct mdp_display_commit)
+#define MSMFB_METADATA_GET  _IOW(MSMFB_IOCTL_MAGIC, 166, struct msmfb_metadata)
+
 #define FB_TYPE_3D_PANEL 0x10101010
 #define MDP_IMGTYPE2_START 0x10000
 #define MSMFB_DRIVER_VERSION	0xF9E8D701
@@ -484,7 +486,23 @@ struct msmfb_mdp_pp {
 		struct mdp_bl_scale_data bl_scale_data;
 	} data;
 };
+ 
+enum {
+  	metadata_op_none,
+  	metadata_op_base_blend,
+ 	metadata_op_frame_rate,
+  	metadata_op_max
+  };
 
+struct msmfb_metadata {
+ 	uint32_t op;
+  	uint32_t flags;
+  	union {
+  		struct mdp_blend_cfg blend_cfg;
+                 uint32_t panel_frame_rate;
+  	} data;
+  };
+  
 #define MDP_MAX_FENCE_FD	10
 #define MDP_BUF_SYNC_FLAG_WAIT	1
 
