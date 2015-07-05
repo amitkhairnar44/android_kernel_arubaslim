c40fbe4f2cfaa8091b0d2eb2836d5fa08e7dabf1
Anonim47 <yosefadis@hotmail.com>
2015-07-05 14:24:31 +0700

    sync: driver commits - Revert "base: sync: signal a sync pt when not adding to the active_list" - sync: Fix error paths - sync: add tracepoint support - sync: refactor sync debug printing - sync: use proper barriers when waiting indefinitely - sync: update new fence status with sync_fence_signal_pt - sync: dump sync state of fence errors - sync: protect unlocked access to fence status - base: sync: signal a sync pt when not adding to the active_list - sync: improve timeout dumps - sync: use correct signed type when handling SYNC_IOC_WAIT - sync: dump sync state to console on timeout - sync: clean up compiler warnings - sync: fix erase-o in sync_fence_wait - sync: change wait timeout to mirror poll semantics - sync: add reference counting to timelines - sync: add internal refcounting to fences - sync: optimize fence merges - sync: reorder sync_fence_release - sync: export sync API symbols - sync: allow async waits to be canceled
    Change-Id: Id65ecdd395782d487773bdf7baa3ed681a72b729
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>
    
    Conflicts:
    
    	drivers/base/sync.c
    
    Signed-off-by: Anonim47 <yosefadis@hotmail.com>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -96,6 +96,8 @@ static int pdev_list_cnt;
 
 int vsync_mode = 1;
 
+int i, ret = 0;
+
 #define MAX_BLIT_REQ 256
 
 #define MAX_FBI_LIST 32
@@ -1979,7 +1981,7 @@ static int msm_fb_release(struct fb_info *info, int user)
 
 int msm_fb_wait_for_fence(struct msm_fb_data_type *mfd)
 {
-	int i, ret = 0;
+	int i, ret;
 	/* buf sync */
 	for (i = 0; i < mfd->acq_fen_cnt; i++) {
 		ret = sync_fence_wait(mfd->acq_fen[i], WAIT_FENCE_TIMEOUT);
@@ -2051,7 +2053,7 @@ static void bl_workqueue_handler(struct work_struct *work)
 	}
 }
 
-static int msm_fb_pan_display(struct fb_var_screeninfo *var,
+static int msm_fb_pan_display_ex(struct fb_var_screeninfo *var,
 			      struct fb_info *info, u32 wait_for_finish)
 {
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
@@ -2184,6 +2186,7 @@ static int msm_fb_pan_display_sub(struct fb_var_screeninfo *var,
 	down(&msm_fb_pan_sem);
 
 	/* buf sync */
+	//int i, ret = 0;
 	for (i = 0; i < mfd->acq_fen_cnt; i++) {
 		ret = sync_fence_wait(mfd->acq_fen[i], WAIT_FENCE_TIMEOUT);
 		sync_fence_put(mfd->acq_fen[i]);
