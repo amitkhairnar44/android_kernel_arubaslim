4f8d371b1200700925b6607775b908a502f0d7a6
Kyle Harrison <khwebmail@gmail.com>
2015-09-09 20:55:22 +0100

    Increase SurfaceFlinger memory by 8mb

--- arch/arm/mach-msm/board-msm7x27a.c
+++ arch/arm/mach-msm/board-msm7x27a.c
@@ -1441,7 +1441,7 @@ static struct platform_device msm8625q_i2c_gpio = {
 };
 
 #ifdef CONFIG_ARCH_MSM7X27A
-#define MSM_RESERVE_MDP_SIZE       0x2300000
+#define MSM_RESERVE_MDP_SIZE       0x2B00000
 #define MSM7x25A_MSM_RESERVE_MDP_SIZE       0x1500000
 #define CAMERA_ZSL_SIZE		(SZ_1M * 60)
 
